14b0b7b9f8a42dd023d8090002d2393bd7451d15
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-25 11:35:21 +0200

    Update TailwindCSS configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles/tailwind.css';
+import './tailwind.css';
+
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
