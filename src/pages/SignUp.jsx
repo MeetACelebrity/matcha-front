224135efd5885091fde8694351e6801befc0a08e
Remy CRESTEY <rcrestey@student.42.fr>
2019-09-26 11:03:03 +0200

    feat(sign-up): begin style of sign-up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,19 +6,28 @@ export default function SignUp() {
 
         e.preventDefault();
     }
+    const inputClass = "border rounded shadow mb-2";
+    const labelClass = "block my-2 focus:shadow-outline";
 
     return (
+
         <section className="flex justify-center">
             <article className="flex justify-center flex-wrap w-64 mt-10 shadow-xl rounded-lg bg-white">
-                <h2 className="w-full text-center py-2 mx-6 border-b border-gray-400 text-gray-900 font-bold">
+                <h2 className="w-full text-center py-2 mx-6 my-3 border-b border-gray-400 text-gray-900 font-bold">
                     Sign Up
                 </h2>
                 <form
                     className="flex flex-col items-center pb-2"
                     onSubmit={onSubmit}
                 >
-                    <input type="text" />
-                    <input type="password" />
+                    <div>
+                        <label className={ labelClass } >Username</label>
+                        <input className={ inputClass } type="text" />
+                    </div>
+                    <div>
+                        <label className={ labelClass } >Password</label>
+                        <input className={ inputClass } type="password" />
+                    </div>
                     <button type="submit">Sign up</button>
                 </form>
             </article>
