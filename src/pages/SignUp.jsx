332cad3973516a98c0615822a5afc5d751c6109e
Remy CRESTEY <rcrestey@student.42.fr>
2019-09-29 01:01:12 +0200

    feat(send data to api): send data with fetching right url  and Improve components :tadat:

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from 'react';
 import FormField from '../components/FormField.jsx';
 
-
 export default function SignUp() {
+
     function onSubmit(e) {
+        const apiUrl = "http://e2r3p10.42.fr";
+        const apiPort = "8080";
+        const apiRoute = "/auth/sign-up";
+
         e.preventDefault();
         console.log(JSON.stringify(data));
-        console.log(data);
+        fetch(apiUrl + ':' + apiPort + apiRoute, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(res => res.json())
+            .then(response => console.log(response));
     }
 
+
+    /**
+     * HOW TO ADD A FIELD ?
+     *      1: add name of your field in the hooks
+     *      2: add name of your field in "fields" array
+     *      3: Add the text that will be print for yout field in "labelName"
+     *      4: Done, you create a new field ready to send to the back
+     */
 
     //hooks
     const [data, setData] = useState({ email: '', username: '', givenName: '', familyName: '', password: '' });
 
-
     //Field Generator
-    const field = ['email', 'username', 'givenName', 'familyName', 'password'];
+    const fields = ['email', 'username', 'givenName', 'familyName', 'password'];
+    const labelNames = ['Email', 'Username', 'First Name', 'Last Name', 'Password']
 
-    const listField = field.map((fieldName) => {
+    const listField = fields.map((fieldName) => {
         return (
-            <FormField key={fieldName} labelName={fieldName} name={fieldName} defaultValue={data[fieldName]} onChange={e => setData({ ...data, [fieldName]: e.target.value })
+            <FormField key={fieldName} labelName={labelNames[fields.indexOf(fieldName)]} name={fieldName} defaultValue={data[fieldName]} onChange={e => setData({ ...data, [fieldName]: e.target.value })
             } />
         );
     })
@@ -29,7 +47,7 @@ export default function SignUp() {
             <article className="flex justify-center flex-wrap w-64 mt-10 shadow-xl rounded-lg bg-white">
                 <h2 className="w-full text-center py-2 mx-6 my-3 border-b border-gray-400 text-gray-900 font-bold">
                     Sign Up
-                </h2>
+                    </h2>
 
                 <form
                     className="flex flex-col items-center pb-2"
@@ -40,6 +58,5 @@ export default function SignUp() {
                 </form>
             </article>
         </section>
-
-    );
+    )
 }
