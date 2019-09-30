88dfc5d1f0802b2683f1440d7e8d8bcc6f1eadf1
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-30 11:53:26 +0200

    feat(tailwind): add styled-components with tailwind last version support

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import NavBar from './components/NavBar.jsx';
 import BottomBar from './components/BottomBar.jsx';
 import Routes from './Routes.jsx';
 
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+const Button = styled.button`
+    ${tw`w-64 bg-blue-700`}
+
+    ${props => props.hover && tw`bg-blue-300`}
+`;
+
 export default function App() {
     return (
         <Router>
             <div className="w-screen min-h-screen bg-gray-100">
                 <NavBar />
+                <Button>TailwindCSS</Button>
 
                 <Routes />
 
