6cb68fa94a8506d53b98408f7350a767e5e439c1
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-10-22 11:20:39 +0200

    improvement(user-edit-pictures): listen to file changes, show the taken picture and upload it

--- src/components/UserProfileModifyPictures.jsx
+++ src/components/UserProfileModifyPictures.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
@@ -11,20 +11,43 @@ const PicturesContainer = styled.div`
 `;
 
 export default function UserProfileModifyPictures() {
-    const [pictures] = useState([
+    const [reader] = useState(new FileReader());
+    const [pictures, setPictures] = useState([
         { src: 'https://picsum.photos/536/354' },
         { src: 'https://picsum.photos/536/355' },
         { src: 'https://picsum.photos/536/356' },
     ]);
 
+    useEffect(() => {
+        reader.onload = ({ target: { result } }) => {
+            setPictures([...pictures, { src: result }]);
+        };
+    }, [reader, setPictures, pictures]);
+
+    function onFileChange({
+        target: {
+            files: [file],
+        },
+    }) {
+        reader.readAsDataURL(file);
+
+        // send the file to the API
+    }
+
     return (
         <UserProfileModifyEditionGroup title="Profile Pictures" noButton>
             <PicturesContainer>
                 {pictures.map(({ src }, i) => (
-                    <Picture src={src} alt="Profile" key={i} />
+                    <Picture
+                        src={src}
+                        alt="One of my profile picture"
+                        key={i}
+                    />
                 ))}
 
-                {pictures.length < 4 && <AddPictureButton />}
+                {pictures.length < 4 && (
+                    <AddPictureButton onChange={onFileChange} />
+                )}
             </PicturesContainer>
         </UserProfileModifyEditionGroup>
     );
