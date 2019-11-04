a141940f98153960baa8706ac07dd743bca58558
Remy CRESTEY <rcrestey@student.42.fr>
2019-11-04 19:15:24 +0100

    improvement(tags): use the propositions stored in the database :tada:

--- src/components/UserProfileModifyTags.jsx
+++ src/components/UserProfileModifyTags.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { API_ENDPOINT } from '../constants';
 
@@ -11,22 +11,18 @@ export default function UserProfileModifyTags({
     context,
     setContext,
 }) {
-    const propositions = [
-        { uuid: 'adfgqertawr23dfg', value: 'TypeScript', text: 'TypeScript' },
-        { uuid: 'adfgqertadfg', value: 'CSS', text: 'CSS' },
-        { uuid: 'adfafgdfgadfg', value: 'France', text: 'France' },
-        { uuid: 'adfgqetadfg', value: 'Humour', text: 'Humour' },
-        { uuid: 'adfadxvcbfga245dfg', value: 'Business', text: 'Business' },
-        {
-            uuid: '54654',
-            value: 'Marketing Mania',
-            text: 'Marketing Mania',
-        },
-        { uuid: '0d4957aa-3612-4f41-8d78-a0e124de67fb', text: 'Golang' },
-        { uuid: 'bd0c5330-a174-47ba-ba30-ad440098914e', text: 'Sex' },
-        { uuid: '6e5f748d-dd84-4c02-b28d-bc1b9fad96c9', text: 'Coca-cola' },
-        { uuid: 'ef5dc4a0-8834-4450-b583-617aa97da04e', text: 'JavaScript' },
-    ];
+    const [propositions, setPropositions] = useState([]);
+
+    useEffect(() => {
+        fetch(`${API_ENDPOINT}/profile/tags`, {
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(propositions => {
+                setPropositions(propositions);
+            })
+            .catch(console.error);
+    }, []);
 
     function addTag(tag) {
         console.log('add tag lol ?', tag);
