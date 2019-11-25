fe8394311a676970a2a84f1444cc4298c7624a03
Devessier <baptiste@devessier.fr>
2019-11-25 19:38:35 +0100

    improvement(notifications): start to design the page and the list

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,5 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+
+import RelativeTime from '../components/RelativeTime.jsx';
+
+const Container = styled.article`
+    ${tw`mx-auto px-5 w-full mt-6`}
+
+    @media (min-width: 768px) {
+        ${tw`w-3/5`}
+    }
+`;
+
+const Title = styled.h2`
+    font-family: 'Saira', sans-serif;
+
+    ${tw`text-3xl mb-4`}
+`;
+
+const Notification = styled.div`
+    ${tw`flex items-center bg-white px-3 py-2 cursor-pointer`}
+
+    transition: background-color 200ms;
+
+    &:first-of-type {
+        ${tw`rounded-tl rounded-tr`}
+    }
+
+    &:not(:last-of-type) {
+        ${tw`border-b border-gray-400`}
+    }
+
+    &:last-of-type {
+        ${tw`rounded-bl rounded-br`}
+    }
+
+    &:hover {
+        ${tw`bg-gray-300`}
+    }
+`;
+
+const NotificationTitle = styled.h3``;
+
+const NotificationDescription = styled.p``;
+
+const NotificationDate = styled(RelativeTime)`
+    ${tw`text-red`}
+`;
+
+const d = +new Date();
 
 export default function Notifications() {
-    return <div>Notifications</div>;
+    const [notifications] = useState([
+        {
+            uuid: 'qrdgjqdfhgjdqfg',
+            title: 'LOOOL',
+            description: 'Description de la notification',
+            createdAt: d,
+        },
+    ]);
+
+    return (
+        <Container>
+            <Title>Notifications</Title>
+
+            {notifications.map(({ uuid, title, description, createdAt }) => (
+                <Notification key={uuid}>
+                    <NotificationTitle>{title}</NotificationTitle>
+
+                    <NotificationDescription>
+                        {description}
+                    </NotificationDescription>
+
+                    <NotificationDate datetime={createdAt} />
+                </Notification>
+            ))}
+        </Container>
+    );
 }
